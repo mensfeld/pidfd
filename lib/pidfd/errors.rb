b182c19839a6a2a49ad39db28c02b388e5089aae
Maciej Mensfeld <maciej@mensfeld.pl>
2025-12-10 14:28:31 +0100

    Update yard-lint to 1.3.0 and fix lint offenses
    - Update yard-lint gem from 1.2.3 to 1.3.0
    - Run yard-lint --update to add 8 new validators to .yard-lint.yml
    - Add missing @api public tags to all public classes, modules, methods, and constants

--- lib/pidfd/errors.rb
+++ lib/pidfd/errors.rb
@@ -2,14 +2,18 @@
 
 class Pidfd
   # Namespace for all the pidfd errors
+  # @api public
   module Errors
     # Base error class for all pidfd errors
+    # @api public
     BaseError = Class.new(StandardError)
 
     # Error raised when pidfd_open syscall fails
+    # @api public
     PidfdOpenFailedError = Class.new(BaseError)
 
     # Error raised when pidfd_signal syscall fails
+    # @api public
     PidfdSignalFailedError = Class.new(BaseError)
   end
 end
--- lib/pidfd/version.rb
+++ lib/pidfd/version.rb
@@ -2,5 +2,6 @@
 
 class Pidfd
   # Current version of the pidfd gem
+  # @api public
   VERSION = '1.0.0'
 end
